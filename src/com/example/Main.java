fc5a143d2ec347caa776db247d1da9bfaf7aaecd
Qsior <lukaszbojes@gmail.com>
2017-12-27 15:34:00 +0100

    Added example of class with encapsulation, used refactoring functionality to change health field to hitPoints field

--- src/com/example/Main.java
+++ src/com/example/Main.java
@@ -3,18 +3,21 @@ package com.example;
 public class Main {
 
     public static void main(String[] args) {
-	    Player player = new Player();
-	    player.fullname = "Tim";
-	    //player.health = 20;
-	    player.weapon = "Sword";
+//	    Player player = new Player();
+//	    player.fullname = "Tim";
+//	    //player.hitPoints = 20;
+//	    player.weapon = "Sword";
+//
+//	    int damage = 10;
+//	    player.looseHealth(damage);
+//        System.out.println("Remaining hitPoints = " + player.healthRemaining());
+//
+//        damage = 11;
+//        player.hitPoints = 200;
+//		player.looseHealth(damage);
+//		System.out.println("Remaining hitPoints = " + player.healthRemaining());
 
-	    int damage = 10;
-	    player.looseHealth(damage);
-        System.out.println("Remaining health = " + player.healthRemaining());
-
-        damage = 11;
-        player.health = 200;
-		player.looseHealth(damage);
-		System.out.println("Remaining health = " + player.healthRemaining());
+        EnhancedPlayer enhancedPlayer = new EnhancedPlayer("Tim", 250, "Sword");
+        System.out.println("Initial hitPoints is " + enhancedPlayer.getHealth());
     }
 }
--- src/com/example/Player.java
+++ src/com/example/Player.java
@@ -3,18 +3,18 @@ package com.example;
 public class Player {
 
     public String fullname;
-    public int health;
+    public int hitPoints;
     public String weapon;
 
     public void looseHealth(int damage){
-        this.health = this.health - damage;
-        if(this.health <= 0) {
+        this.hitPoints = this.hitPoints - damage;
+        if(this.hitPoints <= 0) {
             System.out.println("Player knocked out");
             //reduce number of lives remaining for the player
         }
     }
 
     public int healthRemaining(){
-        return this.health;
+        return this.hitPoints;
     }
 }
